8f935346bade39ce93d002e4909eeb2eda001288
Jenna Braun <braunj@yorku.ca>
2018-08-26 16:22:06 -0400

    calc network indices

--- network.R
+++ network.R
@@ -30,48 +30,8 @@ specieslevel(wide)
 summary(web)
 
 plotweb(wide, method= "cca", labsize = 1.5,text.rot=90, col.low = "green", col.high = "blue", bor.col.high="black", arrow = "down")
-networklevel(wide)
-
-
-
-null.rd2 <- nullmodel(wide, method = "r2dtable")
-null.rd2.sim <- simulate(null.rd2, nsim = 500)
-nulls.vaz <- nullmodel(wide, method = "vaznull", N = 500) 
-nulls.swap <- nullmodel(wide, method = "swap.web", N = 500)
-
-null.rd2.met <- networklevel(null.rd2, index = 'nestedness')
-
-null.t.test(wide, N = 5)
-
-
-
-
-
-#igraph
-bg <- graph_from_incidence_matrix(wide)
-plot(bg, layout=layout.bipartite)
-i_net.com = fastgreedy.community(bg)
-plantmod = i_net.com$membership[1:dim(wide)[1]]
-pollmod = i_net.com$membership[(dim(wide)[1]+1):(length(i_net.com$membership))]
-onet = net[order(plantmod),] 
-onet = t(t(onet)[order(pollmod),])
-i_net = graph_from_incidence_matrix(onet, weight=T)                                                       
-install.packages("wesanderson")
-library(wesanderson)
-com.mem = rbind(as.matrix(sort(plantmod)),as.matrix(sort(pollmod)))
-colrs = wes_palette(name="Darjeeling", 
-                    n=max(i_net.com$mem),
-                    type="continuous")
-
-#Specify vertex and edge parameters
-deg = centr_degree(i_net, mode="all")
-V(i_net)$size=5*sqrt(deg$res)
-V(i_net)$frame.color="white"
-V(i_net)$color=colrs[com.mem]
-V(i_net)$label=NA 
-E(i_net)$width=E(i_net)$weight/10
-
-plot(i_net, layout=layout.circle)
+network.sp <- networklevel(wide)
+network.sp <- as.data.frame(network.sp)
 
 #individual network
 indnet <- visits <- select(visits, Site, uniID, ID, Quantity)
@@ -91,6 +51,13 @@ plotweb(iwide,text.rot = 90)
 
 indices <- specieslevel(iwide)
 indices <- as.data.frame(indices[2])
+network.indiv <- networklevel(iwide)
+
+network.indiv <- as.data.frame(network.indiv)
+calc.indices <- cbind(network.indiv, network.sp)
+write.csv(calc.indices, "Data/Derived Data/networkindices.csv")
+
+
 #join covariaes
 
 indices$uniID <- rownames(indices)
